256ba722d484358dafcc96116118d084498b1de0
lennart.monir <gartenfreund931@aol.com>
2017-12-03 15:55:29 +0100

    Update

--- Grundfahrroboter/src/Main4.java
+++ Grundfahrroboter/src/Main4.java
@@ -1,9 +1,9 @@
-import control.PID;
+import Regler.PID;
+import Sensoren.Lichtsensor;
+import Warten.WartenAuf;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
 import lejos.robotics.RegulatedMotor;
-import sensors.Lichtsensor;
-import wait.WaitFor;
 
 public class Main4 {	
 	public static void main(String[] args) {
@@ -14,7 +14,7 @@ public class Main4 {
 		Lichtsensor licht = new Lichtsensor(1);
 		PID pid = new PID(50, licht, 0.5, 0.2, 0.8, b, c);
 		pid.drivePID(150);
-		WaitFor.Grad(b, 1500, ">=");
+		WartenAuf.Grad(b, 1500, ">=");
 		pid.stopPID();
 		System.exit(0);
 	}
