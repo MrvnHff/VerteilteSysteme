87d443a8352fb077d44112eae9fc24d83a4f800e
Mathias Wittling <mathiaswittling@googlemail.com>
2018-01-20 20:34:51 +0100

    Anfang von Robot controller

--- RoboCop/RoboCop-GUI/src/main/java/de/htwsaar/vs/gui/controller/RobotController.java
+++ RoboCop/RoboCop-GUI/src/main/java/de/htwsaar/vs/gui/controller/RobotController.java
@@ -1,7 +1,19 @@
 package de.htwsaar.vs.gui.controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 public class RobotController implements Initializable{
 
-}
+	@FXML
+	private TextArea textArea;
+	@FXML
+	private Text title;
+	@FXML
+	private TextField position;
+	@FXML
+	private TextField destination;
+} 
